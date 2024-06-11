59d7ed83d698a12c9229c1ad205f4d83291bd9ae
Ulvi Shabanbayli <ulvi.shabanov@gmail.com>
2024-06-11 04:48:57 +0400

    almost done

---
+++ TESTINGDATA.md
@@ -0,0 +1,50 @@
+{
+url - https://mighty.tools/mockmind-api/content/human/49.jpg
+fullName - Adele Rubio
+profession - UX/UI designer
+}
+{
+url - https://mighty.tools/mockmind-api/content/human/65.jpg
+fullName - Adrian English
+profession - Bloger
+}
+{
+url - https://mighty.tools/mockmind-api/content/human/44.jpg
+fullName - Roxie Cuevas
+profession - Dentist
+}
+{
+url - https://mighty.tools/mockmind-api/content/human/5.jpg
+fullName - Gustavo Evans
+profession - FrontEnd developer
+}
+{
+url - https://mighty.tools/mockmind-api/content/human/43.jpg
+fullName - Alejandra Good
+profession - Lawyer
+}
+{
+url - https://mighty.tools/mockmind-api/content/human/60.jpg
+fullName - Kermit Fry
+profession - Teacher
+}
+{
+url - https://mighty.tools/mockmind-api/content/human/76.jpg
+fullName - Karin Bond
+profession - Architect
+}
+{
+url - https://mighty.tools/mockmind-api/content/human/42.jpg
+fullName - Salvador Walton
+profession - Policeman
+}
+{
+url - https://mighty.tools/mockmind-api/content/human/55.jpg
+fullName - Nellie Giles
+profession - Chef
+}
+{
+url - https://mighty.tools/mockmind-api/content/human/4.jpg
+fullName - Charlie Webb
+profession - Carpenter
+}
--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,114 @@
 "use strict";
+
+const baseUrl = "http://localhost:3001";
+
+const grid = document.querySelector(".users__container-grid");
+
+const modal = document.querySelector(".modal");
+const overlay = document.querySelector(".overlay");
+const btnCloseModal = document.querySelector(".close");
+const btnsOpenModal = document.querySelectorAll(".add");
+const btnPost = document.querySelector(".post");
+
+const openModal = function () {
+  modal.classList.remove("hidden");
+  overlay.classList.remove("hidden");
+};
+
+const closeModal = function () {
+  modal.classList.add("hidden");
+  overlay.classList.add("hidden");
+};
+
+for (let i = 0; i < btnsOpenModal.length; i++) {
+  btnsOpenModal[i].addEventListener("click", openModal);
+}
+
+btnCloseModal.addEventListener("click", closeModal);
+
+overlay.addEventListener("click", closeModal);
+
+document.addEventListener("keydown", function (e) {
+  if (e.key === "Escape" && !modal.classList.contains("hidden")) {
+    closeModal();
+  }
+});
+
+const getDataByCB = async (endPoint, cb) => {
+  let response = await fetch(`${baseUrl}/${endPoint}`).then((res) =>
+    res.json()
+  );
+
+  cb(response);
+};
+
+const postData = async (endPoint, data) => {
+  let response = await fetch(`${baseUrl}/${endPoint}`, {
+    method: "POST",
+    body: JSON.stringify(data),
+  });
+  return response;
+};
+
+const deleteData = async (endPoint, id) => {
+  let response = await fetch(`${baseUrl}/${endPoint}/${id}`, {
+    method: "DELETE",
+  });
+  return response;
+};
+
+getDataByCB("data", (datas) => {
+  datas.map((data) => {
+    grid.innerHTML += `
+      <div class="users__container-grid__item">
+        <div>
+          <img
+            src="${data.picsUrl}"
+            alt=""
+          />
+        </div>
+        <div>
+          <div>
+            <h2>${data.name} ${data.surname}</h2>
+            <div class="${data.status}"></div>
+          </div>
+          <p>${data.profession}</p>
+          <button class="delete" data-id="${data.id}">Delete</button>
+        </div>
+      </div>
+    `;
+  });
+
+  const btns = document.querySelectorAll(".delete");
+
+  btns.forEach((btn) => {
+    btn.addEventListener("click", async (e) => {
+      e.preventDefault();
+
+      let attrID = btn.getAttribute("data-id");
+
+      deleteData("data", attrID);
+    });
+  });
+});
+
+const picUrl = document.querySelector("#profile__image");
+const name = document.querySelector("#name");
+const surname = document.querySelector("#surname");
+const profession = document.querySelector("#profession");
+const status = document.querySelector("#status");
+
+btnPost.addEventListener("click", async (e) => {
+  e.preventDefault();
+
+  const userObj = {
+    id: self.crypto.randomUUID(),
+    picsUrl: picUrl.value,
+    name: name.value,
+    surname: surname.value,
+    profession: profession.value,
+    status: status.value,
+  };
+
+  await postData("data", userObj);
+});
